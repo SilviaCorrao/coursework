f5e8a33e757c3d7e511e38cae34cb9215b0296e0
SilviaCorrao <34350578+SilviaCorrao@users.noreply.github.com>
2017-12-07 19:00:08 +0000

    Add files via upload

---
+++ unsemantic-master/index.js
@@ -0,0 +1,20 @@
+/*
+  // This file exists for use with NPM and React.
+  // It is not meant to be loaded into a browser.
+
+  // Example...
+
+  import { Grid, GridContainer } from 'unsemantic'
+*/
+
+// Import.
+import Grid from './assets/react/grid'
+import GridClear from './assets/react/grid_clear'
+import GridContainer from './assets/react/grid_container'
+import GridOffset from './assets/react/grid_offset'
+
+// Export.
+export { Grid }
+export { GridClear }
+export { GridContainer }
+export { GridOffset }
